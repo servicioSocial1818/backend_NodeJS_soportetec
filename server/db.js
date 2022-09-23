77a86b3d54666929c00a157e8ce0aa5f36f25d25
serviciosocial <alumbradopublicoss@gmail.com>
2022-09-23 17:32:11 -0500

    database configuration

---
+++ server/db.js
@@ -0,0 +1,12 @@
+import { createPool } from 'mysql2/promise'
+
+export const pool = new createPool({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'soporteTec'
+})
+
+
+
