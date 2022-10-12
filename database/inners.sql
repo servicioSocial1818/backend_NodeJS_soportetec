d606507b5599f6c382734012880893ef576ad998
serviciosocial <alumbradopublicoss@gmail.com>
2022-10-12 16:57:57 -0500

    inner username

--- database/inners.sql
+++ database/inners.sql
@@ -2,3 +2,6 @@
     SELECT Assignments.idAssignment, Users.first_name, Users.paternal_surname, Users.maternal_surname, Assignments.manager, Users.idUser
 	FROM Users 
     LEFT OUTER JOIN Assignments ON Assignments.idAssignment = Users.idUser WHERE Assignments.idAssignment IS NULL;
+
+-- solo trae username
+SELECT  Users.username FROM Users LEFT OUTER JOIN Assignments ON Assignments.idAssignment = Users.idUser WHERE Assignments.idAssignment IS NULL;
